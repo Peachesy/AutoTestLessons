90f59f17fc6d58ee0ba1102c2031ee075c5c37d6
Peachesy <2360950972@qq.com>
2021-06-02 16:37:43 +0800

    Finished the first coding homework

--- Homeworks/TurnBasedGame.py
+++ Homeworks/TurnBasedGame.py
@@ -9,10 +9,13 @@ enemy_final_hp = enemy_hp - my_power
 英雄最终的⾎量和敌⼈最终的⾎量 ，两个 hp 进⾏对⽐，⾎量剩余多的⼈获胜。
 如果英雄赢了打印 “英雄获胜”，如果敌⼈赢了，打印“敌⼈获胜”
 """
-# 定义敌人类
+
+import random
+
+# 定义英雄类timo
 class timo:
-    hp = 10000  # 代表血量
-    power = 800   # 代表攻击力
+    hp = 10000  # 血量默认值
+    power = 800   # 攻击力默认值
 
     def __init__(self,hp,power):    # 构造函数，分别把类变量传进来
         self.hp = hp
@@ -20,35 +23,77 @@ class timo:
         return
 
     def fight(self,enemy_hp,enemy_power):
-        my_hp = self.hp - enemy_hp
         """
-        :param enemy_hp:
-        :param enemy_power:
-        :return:
+        :param enemy_hp: 敌人的血量
+        :param enemy_power: 敌人的攻击力
+        :return: null
         """
+        # 英雄最终的⾎量 = 英雄hp属性-敌⼈的攻击⼒enemy_power
+        self.hp = self.hp - enemy_power
+
+        #敌⼈最终的⾎量 = 敌⼈的⾎量enemy_hp - 英雄的power属性
+        enemy_hp = enemy_hp - self.power
+
+        if (self.hp > 0) or (enemy_hp > 0):
+            if self.hp > enemy_hp:
+                print("英雄获胜")
+            elif self.hp < enemy_hp:
+                print("敌人获胜")
+            else:
+                print("平局")
+        elif self.hp == 0 and enemy_hp == 0:  # 两位英雄的血量同时为0，证明二者的初始血量和攻击力完全一致
+            print("平局")
         return
 
-# 定义英雄类
+# 定义英雄类police
 class police:
-    hp = 9000
-    power = 700
+    hp = 9000  # 血量默认值
+    power = 700  # 攻击力默认值
 
-    def __init__(self):
+    def __init__(self,hp,power):
+        self.hp = hp
+        self.power = power
         return
 
     def fight(self, enemy_hp, enemy_power):
         """
-        :param enemy_hp:
-        :param enemy_power:
-        :return:
+        :param enemy_hp: 敌人的血量
+        :param enemy_power: 敌人的攻击力
+        :return: null
         """
+        # 英雄最终的⾎量 = 英雄hp属性-敌⼈的攻击⼒enemy_power
+        self.hp = self.hp - enemy_power
+
+        # 敌⼈最终的⾎量 = 敌⼈的⾎量enemy_hp - 英雄的power属性
+        enemy_hp = enemy_hp - self.power
+
+        if (self.hp > 0) or (enemy_hp > 0) :
+            if self.hp > enemy_hp:
+                print("英雄获胜")
+            elif self.hp < enemy_hp:
+                print("敌人获胜")
+            else: print("平局")
+        elif self.hp == 0 and enemy_hp == 0:  # 两位英雄的血量同时为0，证明二者的初始血量和攻击力完全一致
+            print("平局")
         return
 
 
 def main():
-    # my_hp =
-    # my_power =
-    return
+    timo_hp = int(input("请输入timo的初始血量："))
+    timo_power = int(input("请输入timo的初始攻击力："))
+    police_hp = int(input("请输入police的初始血量："))
+    police_power = int(input("请输入police的初始攻击力："))
+    CampA = timo(timo_hp,timo_power)
+    CampB = police(police_hp,police_power)
+    random_key = random.randint(1,2)
+    # print(random_key)
+    if random_key == 1: # key为1时timo为英雄，police为敌人
+        print("本局timo为英雄，police为敌人")
+        CampA.fight(CampB.hp,CampB.power)
+    elif random_key == 2: # key为2时police为英雄，timo为敌人
+        print("本局police为英雄，timo为敌人")
+        CampB.fight(CampA.hp, CampA.power)
+
 
 if __name__ == '__main__':
     main()
