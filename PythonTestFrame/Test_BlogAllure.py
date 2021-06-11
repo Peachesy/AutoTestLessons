504cc75ddc8f928a5548183a61e28c1709a98cd6
v-yutao1 <v-yutao1@microsoft.com>
2021-06-11 16:07:22 +0800

    Add some allure code.

--- PythonTestFrame/Test_BlogAllure.py
+++ PythonTestFrame/Test_BlogAllure.py
@@ -1,70 +1,62 @@
-import time
-import allure
 import pytest
-from selenium import webdriver
+import allure
 
 
-@allure.feature("百度搜索")  # 测试的功能：百度搜索
-@allure.description("""测试百度搜索的纯文本搜索和域名搜索功能""")
-class Test_Allure:
+@allure.title("title:功能层级的title")
+@allure.feature("登录模块")
+class TestLogin():
 
-    url = "https://www.baidu.com/"
-    driver = webdriver.Chrome("D:\Applications\WebDrivers")
-    driver.get(url)
+    @allure.title("title:登录成功测试用例")
+    @allure.story("Story:登录成功")
+    @allure.link("www.baidu.com",name="测试用例地址")
+    @allure.description("这是一个测试用例描述")
+    def test_login_success(self):
+        print("登录成功测试用例")
+        pass
 
-    # def OpenBrowsers(self):
+    @allure.title("title:登录失败测试用例")
+    @allure.story("Story:登录失败")
+    @allure.description_html("""
+    <h1>这是一个用HTML来描述的测试用例</h1>
+    <table style="width:100%">
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Age</th>
+      </tr>
+      <tr align="center">
+        <td>William</td>
+        <td>Smith</td>
+        <td>50</td>
+      </tr>
+      <tr align="center">
+        <td>Vasya</td>
+        <td>Jackson</td>
+        <td>94</td>
+      </tr>
+    </table>
+    """)
+    def test_login_fail(self):
+        with allure.step("点击用户名"):
+            print("输入用户名")
+        with allure.step("点击密码"):
+            print("输入错误的密码")
+        with allure.step("点击登录"):
+            print("点击登录")
+        print("登录失败测试用例")
 
+        # 把部分HTML放到测试报告附件中
+        allure.attach('<head></head><body>登录失败</body>', 'Attach with HTML type',attachment_type=allure.attachment_type.HTML)
+        pass
 
-    @allure.story("纯文本搜索")
-    @allure.link("https://www.baidu.com/", name="百度首页")
-    @allure.description("""该方法主要是在百度搜索中搜索一个关键词并对结果截屏保存，最后生成测试报告""")  # 添加测试描述
-    @pytest.mark.parametrize('keywords',['allure','pytest','selenium'])  # pytest参数化用例
-    def test_search_text(self,keywords):
-        with allure.step("打开百度首页"):  # step方法和with一起使用，方便区分不同的step
-            driver = webdriver.Chrome("D:\Applications\WebDrivers")
-            url = "https://www.baidu.com/"
-            driver.get(url)
+    @allure.title("title:登录时用户名缺失测试用例")
+    @allure.story("Story:用户名缺失")
+    def test_lossUserName(self):
+        print("用户名缺失测试用例")
+        pass
 
-        with allure.step(f"搜索关键词:{keywords}"):  # 在纯文本描述中引入变量
-            driver.find_element_by_id("kw").send_keys(keywords)
-            time.sleep(2)
-            driver.find_element_by_id("su").click()
-            time.sleep(2)
-
-        with allure.step("保存截图"):
-            driver.save_screenshot("./Result/sreenshot.png")
-            # 把截图放到测试报告中
-            allure.attach.file('./Result/screenshot.png',attachment_type=allure.attachment_type.PNG)
-            # 把部分HTML放到测试报告中
-            allure.attach('<head></head><body>首页</body>','Attach with HTML type',attachment_type=allure.attachment_type.HTML)
-
-        with allure.step("关闭浏览器"):
-            driver.quit()
-
-    @allure.story("域名搜索")
-    @allure.link("https://www.baidu.com/",name="百度首页")
-    @allure.description("""该方法主要是在百度搜索中搜索一个域名并对结果截屏保存，最后生成测试报告""")  # 添加测试描述
-    @pytest.mark.parametrize('keywords', ['site:csdn.net'])  # pytest参数化用例
-    def test_search_domain(self,keywords):
-        with allure.step("打开百度首页"):  # step方法和with一起使用，方便区分不同的step
-            driver = webdriver.Chrome("D:\Applications\WebDrivers")
-            url = "https://www.baidu.com/"
-            driver.get(url)
-
-        with allure.step(f"搜索关键词:{keywords}"):  # 在纯文本描述中引入变量
-            driver.find_element_by_id("kw").send_keys(keywords)
-            time.sleep(2)
-            driver.find_element_by_id("su").click()
-            time.sleep(2)
-
-        with allure.step("保存截图"):
-            driver.save_screenshot("./Result/sreenshot.png")
-            # 把截图放到测试报告中
-            allure.attach.file('./Result/screenshot.png', attachment_type=allure.attachment_type.PNG)
-            # 把部分HTML放到测试报告中
-            allure.attach('<head></head><body>首页</body>', 'Attach with HTML type',
-                          attachment_type=allure.attachment_type.HTML)
-
-        with allure.step("关闭浏览器"):
-            driver.quit()
-
+    @allure.title("title:登录时密码缺失测试用例")
+    @allure.story("Story:密码缺失")
+    def test_lossPassword(self):
+        print("缺失密码测试用例")
+        pass
