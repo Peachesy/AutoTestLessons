15694c8f7935e1e05a664ce9144f6baf74cf0758
v-yutao1 <v-yutao1@microsoft.com>
2021-11-10 14:41:25 +0800

    Some exercises

--- Third_WebAutoTest/base.py
+++ Third_WebAutoTest/base.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 
 
-class Base():
-def setup(self):
-    self.driver = webdriver.Chrome()
-    self.driver.maximize_window()
-    self.driver.implicitly_wait(3)
+class Base:
+
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
 
 
-def teardown(self):
-    self.driver.quit()
+    def teardown(self):
+        self.driver.quit()
+
+    def test(self):
+        print("我是Base类的test方法")
--- Third_WebAutoTest/test_alert.py
+++ Third_WebAutoTest/test_alert.py
@@ -1,5 +1,32 @@
 import selenium
+from selenium import webdriver
+from time import sleep
+import pytest
 
-class TestAlert(Base):
 
-    # https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable
+class TestAlert:
+
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
+
+
+    def teardown(self):
+        self.driver.quit()
+
+
+    def test_alert(self):
+        self.driver.get("https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
+        self.switch_to.frame("iframeResult")
+
+        drag = self.driver.find_element_by_id("draggable")
+        drop = self.driver.find_element_by_id("droppable")
+        action = ActionChains(self.driver)
+        action.drag_and_drop(drag,drop).perform()
+        sleep(2)
+
+        self.driver.switch_to.alert.accept()
+        self.driver.switch_to.default_content()
+        self.driver.find_element_by_id("submitBTN").click()
+        sleep(3)
